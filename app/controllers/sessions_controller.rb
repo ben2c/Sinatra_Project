fca6cb055da6defd38bb69c7a6f83acc7a086d9e
ben2c <bencheng200@gmail.com>
2021-01-30 13:41:37 -0600

    updated sessionscontroller with post login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,19 @@ class SessionsController < ApplicationController
         erb :'session/login'
     end
 
+    post '/login' do 
+        begin
+            authenticate(params[:email], params[:password])
+            redirect '/home'
+        rescue AuthError => e
+            @errors = ["Wrong info entered, please try with correct email and/or password"]
+            erb :'session/login'
+        end
+    end
+    
     get '/signup' do
         erb :'session/signup'
     end
     
+
 end
