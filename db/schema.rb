1fef917d8f9b54e533d35e20c6500c4a3fcce4b8
ben2c <bencheng200@gmail.com>
2021-02-08 17:02:56 -0600

    updated password to password_digest in db

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_221918) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
